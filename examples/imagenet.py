e426d7c856ef85f46e58e193fe31d0db917fca47
moskomule <hataya@keio.jp>
2018-04-22 00:32:32 +0900

    add extensions for imagenet

---
+++ examples/imagenet.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import torch
+from torch import optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.models import densenet121
+
+from homura.utils import callbacks, reporter, Trainer
+from homura.data import ImageFolder
+
+
+def imagenet_loader(root, batch_size, num_workers=16):
+    root = Path(root).expanduser()
+    if not root.exists():
+        raise FileNotFoundError
+
+    _normalize = ((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    transform_base = [transforms.ToTensor(),
+                      transforms.Normalize(*_normalize)]
+    transfrom_train = transforms.Compose([transforms.RandomResizedCrop(224),
+                                          transforms.RandomHorizontalFlip()] + transform_base)
+    transfrom_test = transforms.Compose([transforms.Resize(256),
+                                         transforms.CenterCrop(224)] + transform_base)
+
+    train_set = ImageFolder(root / "train", transform=transfrom_train, pre_load=True)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+    test_loader = DataLoader(ImageFolder(root / "val", transform=transfrom_test, pre_load=True),
+                             batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+    return train_loader, test_loader
+
+
+def main(root, epochs, batch_size):
+    train_loader, test_loader = imagenet_loader(root, batch_size)
+    model = torch.nn.DataParallel(densenet121(), device_ids=(0, 1, 2, 3))
+    optimizer = optim.SGD(params=model.parameters(), lr=1e-1, momentum=0.9,
+                          weight_decay=1e-4)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [50, 70])
+    c = callbacks.CallbackList(callbacks.AccuracyCallback(), callbacks.LossCallback(),
+                               callbacks.WeightSave("checkpoints"))
+    r = reporter.TQDMReporter(range(epochs))
+    with callbacks.ReporterCallback(r, c) as rep:
+        trainer = Trainer(model, optimizer, F.cross_entropy, callbacks=rep,
+                          scheduler=scheduler)
+        for _ in r:
+            trainer.train(train_loader)
+            trainer.test(test_loader)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    p = argparse.ArgumentParser()
+    p.add_argument("root")
+    p.add_argument("--epochs", default=1)
+    p.add_argument("--batchsize", default=128)
+    args = p.parse_args()
+    main(**vars(args))
---
+++ homura/data/folder.py
@@ -0,0 +1,72 @@
+from typing import Iterable, Dict
+from pathlib import Path
+from multiprocessing import Pool, cpu_count
+from PIL import Image
+
+import torch.utils.data as data
+
+
+def has_allowed_extension(file: Path, extensions: Iterable[str]):
+    return file.suffix.lower() in extensions
+
+
+def find_classes(root: Path):
+    classes = [d for d in root.iterdir() if d.is_dir()]
+    classes.sort()
+    class_to_idx = {d: i for i, d in enumerate(classes)}
+    return classes, class_to_idx
+
+
+def make_dataset(root: Path, class_to_idx: Dict[str, int], extensions: Iterable[str]):
+    images = []
+    for d in [d for d in root.iterdir() if d.is_dir()]:
+        for f in [f for f in d.iterdir() if has_allowed_extension(d, extensions)]:
+            images.append((d / f, class_to_idx[d]))
+    return images
+
+
+class ImageFolder(data.Dataset):
+    IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif']
+
+    def __init__(self, root, transform=None, pre_load=False):
+        classes, class_to_idx = find_classes(root)
+        samples = make_dataset(root, class_to_idx, self.IMG_EXTENSIONS)
+        if len(samples) == 0:
+            raise RuntimeError(f"Found 0 image in subdirectories of {root}.")
+
+        self.root = root
+        self.classes = classes
+        self.class_to_index = class_to_idx
+        self.samples = samples
+        self.length = len(self.samples)
+        self.transforms = transform
+        self.pre_load = pre_load
+        if self.pre_load:
+            self._load_images()
+
+    def __getitem__(self, index):
+        img, target = self.samples[index]
+        if not self.pre_load:
+            img = self.image_loader(img)
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        return img, target
+
+    def __len__(self):
+        return self.length
+
+    def _load_images(self):
+        def load_file(path, idx):
+            return self.image_loader(path), idx
+
+        with Pool(cpu_count() // 2) as pool:
+            samples = pool.imap(load_file, self.samples, chunksize=10)
+
+        self.samples = tuple(samples)
+
+    @staticmethod
+    def image_loader(path):
+        with open(path, 'rb') as f:
+            return Image.open(f).convert("RGB")
