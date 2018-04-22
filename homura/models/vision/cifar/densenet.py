a58f78e6a0b1c5ffcef5a6052681b8a23b72c0d5
moskomule <hataya@keio.jp>
2018-04-23 02:16:20 +0900

    fix initialization

--- homura/models/vision/cifar/densenet.py
+++ homura/models/vision/cifar/densenet.py
@@ -105,7 +105,7 @@ class CIFARDenseNet(nn.Module):
     def initialize(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal(m.weight.data)
+                nn.init.kaiming_normal_(m.weight.data)
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
