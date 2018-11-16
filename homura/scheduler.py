f4e48265c82ec9858f0553fa08c2ca5733fe9b00
moskomule <hataya@keio.jp>
2018-11-16 14:19:12 +0900

    update args of schedulers

--- homura/scheduler.py
+++ homura/scheduler.py
@@ -25,30 +25,34 @@ class Scheduler(metaclass=ABCMeta):
 
 
 class StepLR(Scheduler):
-    def __init__(self, **kwargs):
-        super(StepLR, self).__init__(_StepLR, **kwargs)
+    def __init__(self, step_size, gamma=0.1, last_epoch=-1):
+        super(StepLR, self).__init__(_StepLR, step_size=step_size, gamma=gamma, last_epoch=last_epoch)
 
 
 class MultiStepLR(Scheduler):
-    def __init__(self, **kwargs):
-        super(MultiStepLR, self).__init__(_MultiStepLR, **kwargs)
+    def __init__(self, milestones, gamma=0.1, last_epoch=-1):
+        super(MultiStepLR, self).__init__(_MultiStepLR, milestones=milestones, gamma=gamma, last_epoch=last_epoch)
 
 
 class LambdaLR(Scheduler):
-    def __init__(self, **kwargs):
-        super(LambdaLR, self).__init__(_LambdaLR, **kwargs)
+    def __init__(self, lr_lambda, last_epoch=-1):
+        super(LambdaLR, self).__init__(_LambdaLR, lr_lambda=lr_lambda, last_epoch=last_epoch)
 
 
 class ExponentialLR(Scheduler):
-    def __init__(self, **kwargs):
-        super(ExponentialLR, self).__init__(_ExponentialLR, **kwargs)
+    def __init__(self, gamma, last_epoch=-1):
+        super(ExponentialLR, self).__init__(_ExponentialLR, gamma=gamma, last_epoch=last_epoch)
 
 
 class CosineAnnealingLR(Scheduler):
-    def __init__(self, **kwargs):
-        super(CosineAnnealingLR, self).__init__(_CosineAnnealingLR, **kwargs)
+    def __init__(self, T_max, eta_min=0, last_epoch=-1):
+        super(CosineAnnealingLR, self).__init__(_CosineAnnealingLR, T_max=T_max, eta_min=eta_min, last_epoch=last_epoch)
 
 
 class ReduceLROnPlateau(Scheduler):
-    def __init__(self, **kwargs):
-        super(ReduceLROnPlateau, self).__init__(_ReduceLROnPlateau, **kwargs)
+    def __init__(self, mode='min', factor=0.1, patience=10,
+                 verbose=False, threshold=1e-4, threshold_mode='rel',
+                 cooldown=0, min_lr=0, eps=1e-8):
+        super(ReduceLROnPlateau, self).__init__(_ReduceLROnPlateau, mode=mode, factor=factor, patience=patience,
+                                                verbose=verbose, threshold=threshold, threshold_mode=threshold_mode,
+                                                cooldown=cooldown, min_lr=min_lr, eps=eps)
