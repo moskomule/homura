1ae9768a288c88ebcb5c19cdde0f9d24653007ee
moskomule <hataya@keio.jp>
2018-11-16 14:22:16 +0900

    add test for scheduler

---
+++ test/test_scheduler.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from torch import nn, optim
+
+from homura.scheduler import *
+
+
+class TestSchedulers(TestCase):
+    def test_scheduler(self):
+        model = nn.Linear(10, 10)
+        opt = optim.SGD(model.parameters(), lr=0.1)
+        scheduler = StepLR(step_size=10)
+        scheduler.set_optimizer(opt)
