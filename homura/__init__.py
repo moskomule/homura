edf52e145cdc74e39db97d3ce590c55b7cc1fa5f
moskomule <hataya@keio.jp>
2018-12-08 16:03:17 +0900

    fix import

--- homura/__init__.py
+++ homura/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["utils", "vision", "modules", "data", "optim.py", "lr_scheduler.py", "trainer", "callbacks", "reporter"]
 from homura import utils, vision, modules, optim, lr_scheduler
-from vision import data
+from homura.vision import data
 from homura.utils import trainer, callbacks, reporter
