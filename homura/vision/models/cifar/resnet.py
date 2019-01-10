28e2088a19f7df87c3babc82ede9e3074b93e3ec
moskomule <hataya@keio.jp>
2019-01-10 17:10:39 +0900

    update resnet

--- homura/vision/models/cifar/resnet.py
+++ homura/vision/models/cifar/resnet.py
@@ -85,13 +85,21 @@ class ResNet(nn.Module):
 
         self.initialize()
 
-    def initialize(self):
+    def initialize(self, zero_init_residual=False):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
+
+            # Zero-initialize the last BN in each residual branch,
+            # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+            # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
 
     def _make_layer(self, block, planes, blocks, stride):
 
