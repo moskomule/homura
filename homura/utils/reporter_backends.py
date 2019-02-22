c806c74b45522f15d9f9b8aa170a40c401c0407a
moskomule <hataya@keio.jp>
2019-02-22 18:38:20 +0900

    fix [skip ci]

--- homura/utils/reporter_backends.py
+++ homura/utils/reporter_backends.py
@@ -12,6 +12,7 @@ import torch
 from torchvision.utils import make_grid, save_image as _save_image
 
 import homura
+from homura.liblog import set_tqdm_handler
 from .miscs import get_git_hash
 from .vocabulary import *
 
@@ -137,6 +138,7 @@ class TQDMWrapper(_WrapperBase):
         super(TQDMWrapper, self).__init__(save_dir)
         self.tqdm = tqdm(iterator, ncols=80)
         self._size = len(iterator)
+        set_tqdm_handler()
 
     def __iter__(self):
         for x in self.tqdm:
