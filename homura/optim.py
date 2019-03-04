5ddee81c467fb27d32f9b951fa92e6a646a6069f
moskomule <hataya@keio.jp>
2019-03-04 14:20:20 +0900

    add adaboost

--- homura/optim.py
+++ homura/optim.py
@@ -2,10 +2,12 @@
 
 from abc import ABCMeta
 
-from torch import nn
-from torch.optim import SGD as _SGD, Adam as _Adam, RMSprop as _RMSprop, ASGD as _ASGD, Optimizer as _Optimizer
+from torch import nn, optim as torch_optim
+from torch.optim import Optimizer as _Optimizer
 
-__all__ = ["Optimizer", "Adam", "SGD", "ASGD", "RMSProp"]
+from .utils import _optimizers
+
+__all__ = ["Optimizer", "Adam", "SGD", "ASGD", "RMSProp", "AdaBound"]
 
 
 class Optimizer(metaclass=ABCMeta):
@@ -26,21 +28,30 @@ class Optimizer(metaclass=ABCMeta):
 
 class Adam(Optimizer):
     def __init__(self, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False):
-        super(Adam, self).__init__(_Adam, lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=amsgrad)
+        super(Adam, self).__init__(torch_optim.Adam,
+                                   lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, amsgrad=amsgrad)
 
 
 class SGD(Optimizer):
     def __init__(self, lr, momentum=0, dampening=0, weight_decay=0, nesterov=False):
-        super(SGD, self).__init__(_SGD, lr=lr, momentum=momentum, dampening=dampening, weight_decay=weight_decay,
+        super(SGD, self).__init__(torch_optim.SGD, lr=lr, momentum=momentum, dampening=dampening,
+                                  weight_decay=weight_decay,
                                   nesterov=nesterov)
 
 
 class RMSProp(Optimizer):
     def __init__(self, lr=0.01, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0, centered=False):
-        super(RMSProp, self).__init__(_RMSprop, lr=lr, alpha=alpha, eps=eps, weight_decay=weight_decay,
+        super(RMSProp, self).__init__(torch_optim.RMSprop, lr=lr, alpha=alpha, eps=eps, weight_decay=weight_decay,
                                       momentum=momentum, centered=centered)
 
 
 class ASGD(Optimizer):
     def __init__(self, lr=0.01, lambd=0.0001, alpha=0.75, t0=1000000.0, weight_decay=0):
-        super(ASGD, self).__init__(_ASGD, lr=lr, lambd=lambd, alpha=alpha, t0=t0, weight_decay=weight_decay)
+        super(ASGD, self).__init__(torch_optim.ASGD, lr=lr, lambd=lambd, alpha=alpha, t0=t0, weight_decay=weight_decay)
+
+
+class AdaBound(Optimizer):
+    def __init__(self, lr=1e-3, betas=(0.9, 0.999), final_lr=0.1, gamma=1e-3,
+                 eps=1e-8, weight_decay=0, amsbound=False):
+        super(AdaBound, self).__init__(_optimizers.AdaBound, lr=lr, betas=betas, final_lr=final_lr, gamma=gamma,
+                                       eps=eps, weight_decay=weight_decay, amsbound=amsbound)
