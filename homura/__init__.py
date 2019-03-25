66b7d0063a17c3ff26c776de05f2b08ebc8b721c
moskomule <hataya@keio.jp>
2019-03-25 17:34:12 +0900

    fix

--- homura/__init__.py
+++ homura/__init__.py
@@ -1,8 +1,10 @@
-from homura.utils.environment import __all__ as environment_all
+from homura.utils.environment import *
 
 from . import callbacks, debug, liblog, lr_scheduler, optim, reporters, trainers, metrics, modules, utils, vision
 from .utils import *
 
 __all__ = ["callbacks", "debug", "liblog", "lr_scheduler", "optim", "reporter",
            "reporters", "trainer", "trainers",
-           "metrics", "modules", "utils", "vision", "Map", "TensorTuple"] + environment_all
+           "metrics", "modules", "utils", "vision", "Map", "TensorTuple",
+           "is_apex_available", "is_tensorboardX_available", "is_accimage_available",
+           "enable_accimage"]
