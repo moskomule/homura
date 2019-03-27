6019731c5fb0655b4cad76d2c36ebb746683d0c5
moskomule <hataya@keio.jp>
2019-03-28 01:19:46 +0900

    fix

--- homura/vision/data/prefetcher.py
+++ homura/vision/data/prefetcher.py
@@ -10,6 +10,7 @@ class DataPrefetcher(object):
 
     def __init__(self, loader: DataLoader):
         self._cuda_available = torch.cuda.is_available()
+        self.loader = iter(loader)
         self.stream = torch.cuda.Stream() if self._cuda_available else None
         self.next_data = None
         self.preload()
