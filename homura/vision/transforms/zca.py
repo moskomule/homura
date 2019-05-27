c79a04a885777e006afa6f31321189386e644003
moskomule <hataya@keio.jp>
2019-05-27 19:54:11 +0900

    add zca

---
+++ homura/vision/transforms/zca.py
@@ -0,0 +1,31 @@
+import torch
+from torchvision.transforms import LinearTransformation
+
+
+def zca_statistics(input: torch.Tensor,
+                   eps: float = 1e-3):
+    """ ZCA-whitening
+
+    :param input:
+    :param eps:
+    :return:
+    """
+    if input.dim() != 4:
+        raise RuntimeError(f"Dimension of `input` is expected to be 4 but got {input.dim()}")
+    b, c, h, w = input.size()
+    input = input.reshape(b, -1)
+    mean = input.mean(dim=0)
+    input -= mean
+    cov = input.t().matmul(input) / b
+    u, s, v = cov.svd(compute_uv=True)
+    matrix = (u @ (s + eps).sqrt_().reciprocal_().diag()) @ u.t()
+    return matrix, mean
+
+
+class ZCATransformation(LinearTransformation):
+
+    @staticmethod
+    def zca(input: torch.Tensor,
+            eps: float = 1e-3):
+        matrix, mean = zca_statistics(input, eps)
+        return ZCATransformation(matrix, mean)
