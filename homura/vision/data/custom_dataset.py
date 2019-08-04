fc46270317a343008818a9f968438176c9f7babb
moskomule <hataya@keio.jp>
2019-08-04 13:29:44 +0900

    fix name

--- homura/vision/data/custom_dataset.py
+++ homura/vision/data/custom_dataset.py
@@ -6,23 +6,24 @@ __all__ = ["TransformableSubset", "transformable_random_split"]
 
 
 class TransformableSubset(data.Subset):
-    """ Subset with transform
+    """ Subset with transform. The original `Subset` inherits transformations from the original Dataset,
+    which may not be useful in some cases, e.g. train-validation split.
     """
 
     def __init__(self, dataset, indices):
         super(TransformableSubset, self).__init__(dataset, indices)
-        self.transforms = None
+        self.transform = None
         self.target_transform = None
 
-    def update_transforms(self, transforms, target_transform=None):
-        self.transforms = transforms
+    def update_transforms(self, transform, target_transform=None):
+        self.transform = transform
         self.target_transform = target_transform
 
     def __getitem__(self, idx):
         item = self.dataset[self.indices[idx]]
         img, target = item
-        if self.transforms is not None:
-            img = self.transforms(img)
+        if self.transform is not None:
+            img = self.transform(img)
         if self.target_transform is not None:
             target = self.target_transform(target)
         return img, target
