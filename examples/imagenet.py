0fa610a8eb97504b18444df596059e0660421be7
moskomule <hataya@keio.jp>
2019-11-23 13:22:27 +0900

    fix

--- examples/imagenet.py
+++ examples/imagenet.py
@@ -16,39 +16,34 @@ def main():
     model = resnet50()
     optimizer = optim.SGD(lr=1e-1 * args.batch_size * get_num_nodes() / 256, momentum=0.9, weight_decay=1e-4)
     scheduler = lr_scheduler.MultiStepLR([30, 60, 80])
-    c = [callbacks.AccuracyCallback(), callbacks.LossCallback()]
-    r = reporters.TQDMReporter(range(args.epochs), callbacks=c)
-    tb = reporters.TensorboardReporter(c)
-    rep = callbacks.CallbackList(r, tb, callbacks.WeightSave("checkpoints"))
+    tq = reporters.TQDMReporter(range(args.epochs))
+    c = [callbacks.AccuracyCallback(), callbacks.AccuracyCallback(k=5),
+         callbacks.LossCallback(), tq, reporters.TensorboardReporter(".")]
     _train_loader, _test_loader = imagenet_loaders(args.root, args.batch_size, distributed=args.distributed,
                                                    num_train_samples=args.batch_size * 10 if args.debug else None,
                                                    num_test_samples=args.batch_size * 10 if args.debug else None)
 
     use_multi_gpus = not args.distributed and torch.cuda.device_count() > 1
-    if use_multi_gpus:
-        print("\nuse DataParallel\n")
-    trainer = SupervisedTrainer(model, optimizer, F.cross_entropy, callbacks=rep, scheduler=scheduler,
-                                data_parallel=use_multi_gpus, distributed_backend=args.backend,
-                                init_method=args.init_method)
+    with SupervisedTrainer(model, optimizer, F.cross_entropy, callbacks=c, scheduler=scheduler,
+                           data_parallel=use_multi_gpus, distributed_backend=args.backend,
+                           init_method=args.init_method) as trainer:
 
-    for epoch in r:
-        if args.use_prefetcher:
-            train_loader = prefetcher.DataPrefetcher(_train_loader)
-            test_loader = prefetcher.DataPrefetcher(_test_loader)
-        else:
-            train_loader, test_loader = _train_loader, _test_loader
-        # following apex's training scheme
-        trainer.train(train_loader)
-        trainer.test(test_loader)
-
-    rep.close()
+        for epoch in tq:
+            if args.use_prefetcher:
+                train_loader = prefetcher.DataPrefetcher(_train_loader)
+                test_loader = prefetcher.DataPrefetcher(_test_loader)
+            else:
+                train_loader, test_loader = _train_loader, _test_loader
+            # following apex's training scheme
+            trainer.train(train_loader)
+            trainer.test(test_loader)
 
 
 if __name__ == '__main__':
     import miniargs
     import warnings
 
-    # to suppress annoying warnings
+    # to suppress annoying warnings from PIL
     warnings.filterwarnings("ignore", "(Possibly )?corrupt EXIF data", UserWarning)
 
     p = miniargs.ArgumentParser()
