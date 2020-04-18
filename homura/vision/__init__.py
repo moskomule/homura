586076d2917fd8c89d2bcd1d0b48ec68bcad9fd8
moskomule <hataya@keio.jp>
2020-04-18 11:30:00 +0900

    fix import

--- homura/vision/__init__.py
+++ homura/vision/__init__.py
@@ -1,5 +1,5 @@
-from vision.transforms.cutmix import cutmix, input_cutmix
 from .data import *
 from .data.statistics import PerChannelStatistics
 from .models import *
 from .transforms import *
+from .transforms.cutmix import cutmix, input_cutmix
