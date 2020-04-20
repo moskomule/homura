9494d814d44d9ebb29ea37cbe33facb87032b082
moskomule <hataya@keio.jp>
2020-04-20 14:16:13 +0900

    version up

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().split()
 
 setup(name="homura",
-      version="2020.02.0",
+      version="2020.04.0",
       author="moskomule",
       author_email="hataya@nlab.jp",
       packages=find_packages(exclude=["test", "docs", "examples"]),
