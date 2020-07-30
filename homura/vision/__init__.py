308e0c1e06fe33b602ccb37f033006d92bf2659e
moskomule <hataya@keio.jp>
2020-07-30 18:37:49 +0900

    remove cutmix/cutout

--- homura/vision/__init__.py
+++ homura/vision/__init__.py
@@ -1,6 +1,4 @@
 from .data import *
 from .models import *
 from .transforms import *
-from .transforms.cutmix import cutmix, input_cutmix
-from .transforms.cutout import CutOut, cutout
 from .transforms.mixup import mixup, partial_mixup
--- test/test_vision/test_cutout.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-from homura.vision import CutOut
-
-
-def test_cutout():
-    input = torch.randn(3, 32, 32)
-    cutout = CutOut(16)
-    cutout(input)
