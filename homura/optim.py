7d4e45e01e1758f893213611d9fbf59d050a3656
moskomule <hataya@keio.jp>
2020-08-10 17:27:58 +0900

    add other optimizers

--- homura/optim.py
+++ homura/optim.py
@@ -11,3 +11,13 @@ def Adam(lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
 def SGD(lr=1e-1, momentum=0, dampening=0,
         weight_decay=0, nesterov=False):
     return partial(torch.optim.SGD, **locals())
+
+
+def RMSprop(lr=1e-2, alpha=0.99, eps=1e-8,
+            weight_decay=0, momentum=0, centered=False):
+    return partial(torch.optim.RMSprop, **locals())
+
+
+def AdamW(lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+          weight_decay=1e-2, amsgrad=False):
+    return partial(torch.optim.AdamW, **locals())
