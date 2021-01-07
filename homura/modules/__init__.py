ec6303da1552d0a52b59b95ad5d20ab292da8151
moskomule <hataya@keio.jp>
2021-01-07 16:05:39 +0900

    fix import

--- homura/modules/__init__.py
+++ homura/modules/__init__.py
@@ -1,4 +1,4 @@
 from .attention import KeyValAttention
 from .discretization import *
-from .ema import EMANet, exponential_moving_average_
+from .ema import EMA, exponential_moving_average_
 from .functional import *
