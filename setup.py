dc2b213059a9539bad6fe211af7bb82c5672df79
moskomule <hataya@keio.jp>
2021-01-17 00:31:20 +0900

    fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().split()
 
 setup(name="homura-core",
-      version="2020.12.0",
+      version="2021.01.0",
       author="moskomule",
       author_email="hataya@nlab-mpg.jp",
       packages=find_packages(exclude=["test", "docs", "examples"]),
