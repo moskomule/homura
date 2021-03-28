99b6b23ba3332fe3f721a06bc5f9c5a0f383248b
moskomule <hataya@nlab-mpg.jp>
2021-03-28 17:47:48 +0900

    add a simple distributed helper

--- homura/__init__.py
+++ homura/__init__.py
@@ -2,7 +2,7 @@ from .register import Registry
 from .utils import TensorDataClass, TensorTuple, distributed_print, enable_accimage, get_args, get_environ, \
     get_git_hash, get_global_rank, get_local_rank, get_num_nodes, get_world_size, if_is_master, init_distributed, \
     is_accimage_available, is_distributed, is_distributed_available, is_faiss_available, is_master, set_deterministic, \
-    set_seed
+    set_seed, distributed_ready_main
 
 Registry.import_modules('homura.vision')
 # to avoid circular import
