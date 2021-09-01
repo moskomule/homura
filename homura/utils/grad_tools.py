e7eab0e247cf1a5e075df2b182cbbf8bb6a50f04
moskomule <hataya@nlab-mpg.jp>
2021-09-01 17:53:06 +0900

    improve jvp

--- homura/utils/grad_tools.py
+++ homura/utils/grad_tools.py
@@ -17,12 +17,14 @@ def param_to_vector(parameters: Iterable[torch.Tensor]) -> torch.Tensor:
 
 def vjp(f: torch.Tensor,
         p: nn.Parameter | list[nn.Parameter],
-        v: torch.Tensor
+        v: torch.Tensor,
+        *,
+        only_retain_graph: bool = False
         ) -> torch.Tensor:
     """ vector Jacobian product
     """
 
-    return param_to_vector(grad(f, p, v, create_graph=True))  # dim p
+    return param_to_vector(grad(f, p, v, create_graph=not only_retain_graph, retain_graph=True))  # dim p
 
 
 def jvp(f: torch.Tensor,
@@ -34,7 +36,8 @@ def jvp(f: torch.Tensor,
 
     dummy = torch.ones_like(f, requires_grad=True)
     g = vjp(f, p, dummy)
-    return vjp(g, dummy, v)  # dim y
+    # note that we don't need higher order gradient w.r.t. dummy
+    return vjp(g, dummy, v, only_retain_graph=True)  # dim y
 
 
 def hvp(loss: torch.Tensor,
